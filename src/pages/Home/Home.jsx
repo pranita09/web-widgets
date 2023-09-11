9cb03d23877bb9fdf0bb9180e2a966e5360286ae
Pranita <pfulsundar8@gmail.com>
2023-09-11 18:12:19 +0530

    feat: Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,23 @@
+import { useNavigate } from "react-router-dom";
 import "./Home.css";
 
 export const Home = () => {
+  const navigate = useNavigate();
   return (
-    <>
-      <h1 className="header">Home</h1>
-    </>
+    <div className="home">
+      <div className="description">
+        <p className="tagline1">
+          Simplify, Beautify, and Customize <span>with</span>
+        </p>
+        <p className="tagline2">WebWidgets</p>
+        <p className="info">
+          A modern, sleek and accessible library of css components, enabling you
+          to build your own design system and develop your websites faster.
+        </p>
+        <button className="get-started-btn" onClick={() => navigate("/docs")}>
+          Get Started
+        </button>
+      </div>
+    </div>
   );
 };
